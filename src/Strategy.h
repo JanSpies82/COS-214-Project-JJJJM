08d74610dc852439a6eaa10138e6e1c4ea8491ad
Mekhail Muller <u21632792@tuks.co.za>
2022-10-23 19:49:20 +0200

    Added comments and functions within the Strategy.h file

--- src/Strategy.h
+++ src/Strategy.h
@@ -9,11 +9,24 @@ using namespace std;
 class Country;
 class Strategy;
 
+//! Strategy Class
+/*!
+ * This class determines the decisions that a country makes.
+ */
+
 __abstract class Strategy
 {
-	public: Country* countrys;
+    public: Strategy(Country*);
+    /**
+     * @brief Constructor: initialises Country variable
+     */
+
+	public: Country* countrys; /*!< Holds country in which the strategy belongs to */
 
 	public: virtual void algorithm() = 0;
+    /**
+     * @brief virtual function: Decides on countries next move
+     */
 };
 
 #endif
