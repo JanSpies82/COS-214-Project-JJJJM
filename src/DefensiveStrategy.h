42ac3dfebb9787369b735be5fa229bfad94100d9
Mekhail Muller <u21632792@tuks.co.za>
2022-10-23 20:04:11 +0200

    DefensiveStrategy.h : Added comments and functions

--- src/DefensiveStrategy.h
+++ src/DefensiveStrategy.h
@@ -9,10 +9,18 @@ using namespace std;
 // class Strategy;
 class DefensiveStrategy;
 
+//! DefensiveStrategy Class
+/*!
+ * This class determines the decisions that a country makes with the goal of defending the country
+ */
+
 class DefensiveStrategy: public Strategy
 {
 
 	public: void algorithm();
+    /**
+     * @brief Implements abstract function: Decides on countries next move with the goal of defending the country and its military
+     */
 };
 
 #endif
