0dc950b00330c843dcbf5eb31b9a7fd5b3f79299
DeadpanSplash82 <janco.spies@gmail.com>
2022-10-25 08:52:47 +0200

    Commented out unimplemented methods

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -4,11 +4,11 @@ using namespace std;
 #include "Plane.h"
 #include "Vehicle.h"
 
-void Plane::findEnemy() {
-	throw "Not yet implemented";
-}
+// void Plane::findEnemy() {
+// 	throw "Not yet implemented";
+// }
 
-void Plane::destroyEnemy() {
-	throw "Not yet implemented";
-}
+// void Plane::destroyEnemy() {
+// 	throw "Not yet implemented";
+// }
 
