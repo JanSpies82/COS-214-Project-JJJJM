39b6639afdd575a57705590b701c3e18145b3bfd
DeadpanSplash82 <janco.spies@gmail.com>
2022-10-25 20:33:25 +0200

    Completed Memento implementation

--- src/SimulationState.cpp
+++ src/SimulationState.cpp
@@ -3,5 +3,16 @@ using namespace std;
 
 #include "SimulationState.h"
 #include "SimulationManager.h"
-#include "Memento.h"
 
+SimulationState::SimulationState(SimulationManager *_simulationManager)
+{
+    this->simulationManager = _simulationManager;
+    this->timestamp = time(0);
+}
+
+SimulationState::~SimulationState(){};
+
+time_t SimulationState::getTimestamp()
+{
+    return this->timestamp;
+}
--- test/MementoTest.cpp
+++ test/MementoTest.cpp
@@ -1,9 +1,62 @@
 #include <limits.h>
 #include <stdexcept>
 #include "../src/Memento.h"
+#include "../src/SimulationState.h"
 #include "gtest/gtest.h"
 
-namespace {
+namespace
+{
+    TEST(MementoTest, Constructor)
+    {
+        Memento *memento = new Memento();
+        try
+        {
+            memento->getState();
+            FAIL() << "Expected std::out_of_range";
+        }
+        catch (std::out_of_range const &err)
+        {
+            EXPECT_EQ(err.what(), std::string("Memento does not hold a SimulationState"));
+        }
+        catch (...)
+        {
+            FAIL() << "Expected std::out_of_range";
+        }
+        delete memento;
+    }
 
-    
+    TEST(MementoTest, Destructor)
+    {
+        Memento *memento = new Memento();
+        delete memento;
+    }
+
+    TEST(MementoTest, GetStateNotSet)
+    {
+        Memento *memento = new Memento();
+        try
+        {
+            memento->getState();
+            FAIL() << "Expected std::out_of_range";
+        }
+        catch (std::out_of_range const &err)
+        {
+            EXPECT_EQ(err.what(), std::string("Memento does not hold a SimulationState"));
+        }
+        catch (...)
+        {
+            FAIL() << "Expected std::out_of_range";
+        }
+        delete memento;
+    }
+
+    TEST(MementoTest, GetStateSet)
+    {
+        Memento *memento = new Memento();
+        SimulationState *simulationState = new SimulationState(NULL);
+        memento->setState(simulationState);
+        EXPECT_EQ(memento->getState(), simulationState);
+        delete memento;
+    }
+
 }
