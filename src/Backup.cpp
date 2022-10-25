9652bc6ef9f7efd7246a19f8f406120310ad0dd0
DeadpanSplash82 <janco.spies@gmail.com>
2022-10-25 20:49:17 +0200

    Completed Backup implementation

--- src/Backup.cpp
+++ src/Backup.cpp
@@ -1,5 +1,45 @@
-
 #include "Backup.h"
 #include "SimulationManager.h"
 #include "Memento.h"
+
+using namespace std;
+
+Backup::Backup()
+{
+    this->mementos = new vector<Memento *>();
+}
+
+Backup::~Backup()
+{
+    this->clear();
+    delete this->mementos;
+}
+
+void Backup::addMemento(Memento *_memento)
+{
+    this->mementos->insert(this->mementos->begin(), _memento);
+}
+
+Memento *Backup::getMemento()
+{
+    if (this->mementos->size() == 0)
+        __throw_out_of_range("Backup is empty");
+    
+    Memento *memento = this->mementos->back();
+    this->mementos->pop_back();
+    return memento;
+}
+
+int Backup::getMementoCount()
+{
+    return this->mementos->size();
+}
+
+void Backup::clear()
+{
+    for (int i = 0; i < this->mementos->size(); i++)
+        delete this->mementos->at(i);
+    
+    this->mementos->clear();
+}
 
