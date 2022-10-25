fa7c9f72c295ca3f01a70938048e13c717236075
DeadpanSplash82 <janco.spies@gmail.com>
2022-10-25 08:41:51 +0200

    Commented out unimplemented methods

--- src/Iterator.cpp
+++ src/Iterator.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 #include "Iterator.h"
-void Iterator::first() {
-	throw "Not yet implemented";
-}
+// void Iterator::first() {
+// 	throw "Not yet implemented";
+// }
 
-void Iterator::next() {
-	throw "Not yet implemented";
-}
+// void Iterator::next() {
+// 	throw "Not yet implemented";
+// }
 
-void Iterator::isDone() {
-	throw "Not yet implemented";
-}
+// void Iterator::isDone() {
+// 	throw "Not yet implemented";
+// }
 
-void Iterator::current() {
-	throw "Not yet implemented";
-}
+// void Iterator::current() {
+// 	throw "Not yet implemented";
+// }
 
