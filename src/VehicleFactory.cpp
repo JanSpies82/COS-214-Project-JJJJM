35b5b39b388c262cfc6f0c791a98093d9f7ff5a1
DeadpanSplash82 <janco.spies@gmail.com>
2022-10-25 08:58:10 +0200

    Commented out unimplemented methods

--- src/VehicleFactory.cpp
+++ src/VehicleFactory.cpp
@@ -4,11 +4,11 @@ using namespace std;
 #include "VehicleFactory.h"
 #include "Military.h"
 
-Vehicle* VehicleFactory::factoryMethod() {
-	throw "Not yet implemented";
-}
+// Vehicle* VehicleFactory::factoryMethod() {
+// 	throw "Not yet implemented";
+// }
 
-void VehicleFactory::anOperation() {
-	throw "Not yet implemented";
-}
+// void VehicleFactory::anOperation() {
+// 	throw "Not yet implemented";
+// }
 
