e0e0eb99ff3fb81ff4d62f822ad0f3ddde64b775
DeadpanSplash82 <janco.spies@gmail.com>
2022-10-25 08:56:12 +0200

    Commented out unimplemented methods

--- src/Ship.cpp
+++ src/Ship.cpp
@@ -4,11 +4,11 @@ using namespace std;
 #include "Ship.h"
 #include "Vehicle.h"
 
-void Ship::findEnemy() {
-	throw "Not yet implemented";
-}
+// void Ship::findEnemy() {
+// 	throw "Not yet implemented";
+// }
 
-void Ship::destroyEnemy() {
-	throw "Not yet implemented";
-}
+// void Ship::destroyEnemy() {
+// 	throw "Not yet implemented";
+// }
 
