e5760793321b1cb4ed84bdf15be80bb5d09a219b
jakeweatherhead <jakeweatherhead1.com>
2022-10-25 17:17:56 +0200

    Removed unnecessary auto-generated elements

--- src/CountryMediator.h
+++ src/CountryMediator.h
@@ -1,15 +1,12 @@
 #ifndef __CountryMediator_h__
 #define __CountryMediator_h__
 
-// #include "Country.h"
-
 class Country;
-class CountryMediator;
 
 class CountryMediator
 {
 public: 
-  Country* countries;
+  std::vector<Country*>* countries;
 
   /**
     * @brief for all countries, notify them of the new state
