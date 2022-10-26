9e857e267e3a553d3ab16502f38b1fe72ab5fa3d
DeadpanSplash82 <janco.spies@gmail.com>
2022-10-26 13:23:26 +0200

    Added SimulationState constructor & destructor

--- src/SimulationState.cpp
+++ src/SimulationState.cpp
@@ -11,9 +11,23 @@ SimulationState::SimulationState(SimulationManager *_simulationManager)
 {
     this->simulationManager = _simulationManager;
     this->timestamp = time(0);
+    mapState = NULL;
+    countryStates = new vector<CountryState *>();
+    militaryStates = new vector<MilitaryState *>();
 }
 
-SimulationState::~SimulationState(){};
+SimulationState::~SimulationState(){
+    if (mapState != NULL)
+        delete mapState;
+        
+    for (int i = 0; i < countryStates->size(); i++)
+        delete countryStates->at(i);
+    delete countryStates;
+
+    for (int i = 0; i < militaryStates->size(); i++)
+        delete militaryStates->at(i);
+    delete militaryStates;
+};
 
 time_t SimulationState::getTimestamp()
 {
