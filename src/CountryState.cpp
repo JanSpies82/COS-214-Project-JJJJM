4d48f2e5cab120c65a72f7a318f31bba75f254c0
jakeweatherhead <jakeweatherhead1@gmail.com>
2022-10-27 15:45:48 +0200

    Built parameterised constructor

--- src/CountryState.cpp
+++ src/CountryState.cpp
@@ -4,9 +4,12 @@
 
 CountryState::CountryState(Country* country){
   timestamp = std::time(0);
+  numCitizens = country->getNumCitizens();
+  politicalStability = country->getPoliticalStability();
+  domesticMorale = country->getDomesticMorale();
+  borderStrength = country->getBorderStrength();
+  capitalSafety = country->getCapitalSafety();
+  warSentiment = country->getWarSentiment();
+  tradeRouteSafety = country->getTradeRouteSafety();
 }
-
-// void CountryState::simulationState() {
-// 	return;
-// }
 
