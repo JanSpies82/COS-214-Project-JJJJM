3d216a6ddff04346c0d0ebdea175772f12f0b9f7
DeadpanSplash82 <janco.spies@gmail.com>
2022-10-29 09:34:55 +0200

    Updated Iterator interface

--- src/Iterator.h
+++ src/Iterator.h
@@ -2,67 +2,44 @@
 #define __Iterator_h__
 
 #include <exception>
-#include "Territory.h"
 #include "Location.h"
 
 class Location;
-class Iterator;
 
 class Iterator
 {
 
-	public: 
-		Location* current;
-		
-		/**
-		 * @brief Sets current to the next location in the sequential iteration of the map
-		 * 
-		 */
-		virtual void next() = 0;
+public:
+	/**
+	 * @brief Sets current to the next location in the sequential iteration of the map
+	 * 
+	 * Exceptions : out_of_range if there is no next location
+	 *
+	 */
+	virtual void next() = 0;
 
-		/**
-		 * @brief Sets current to the top left location of the map
-		 * 
-		 */
-		virtual void topLeft() = 0;	
+	/**
+	 * @brief Sets current to the top left location of the map
+	 *
+	 */
+	virtual void first() = 0;
 
-		/**
-		 * @brief Shifts current pointer to it's right neighbour
-		 * 
-		 */
-		virtual void right() = 0;	
+	/**
+	 * @brief Returns true if sequential iteration is complete and false otherwise
+	 *
+	 * @return boolean
+	 */
+	virtual bool isDone() = 0;
 
-		/**
-		 * @brief Shifts current pointer to it's left neighbour
-		 * 
-		 */	
-		virtual void left() = 0;
+	/**
+	 * @brief Returns the pointer to the current location
+	 *
+	 * @return Location*
+	 */
+	virtual Location *getCurrent() = 0;
 
-		/**
-		 * @brief Shifts current pointer to it's bottom neighbour
-		 * 
-		 */		
-		virtual void down() = 0;
-
-		/**
-		 * @brief Shifts current pointer to it's top neighbour
-		 * 
-		 */		
-		virtual void up() = 0;	
-
-		/**
-		 * @brief Returns true if sequential iteration is complete and false otherwise
-		 * 
-		 * @return boolean 
-		 */
-		virtual bool isDone() = 0;	
-			
-		/**
-		 * @brief Returns the pointer to the current location
-		 * 
-		 * @return Location* 
-		 */
-		virtual Location* getCurrent() = 0;
+protected:
+	Location *current;
 };
 
 #endif
--- src/LocationIterator.h
+++ src/LocationIterator.h
@@ -4,65 +4,56 @@
 #include <exception>
 #include "Iterator.h"
 
-class Territory;
 class Location;
-class Iterator;
-class LocationIterator;
 
-class LocationIterator: public Iterator
+class LocationIterator : public Iterator
 {
-	public:
-		Location* current;
-		
-		/**
-		 * @brief Sets current to the next location in the sequential iteration of the map
-		 * 
-		 */
-		void next();
+public:
+	/**
+	 * @brief Construct a new Location Iterator object
+	 *
+	 * @param _location : Location* - pointer to the location that is set as the current location
+	 */
+	LocationIterator(Location* _location);
 
-		/**
-		 * @brief Sets current to the top left location of the map
-		 * 
-		 */
-		void topLeft();	
+	/**
+	 * @brief Destroy the Location Iterator object
+	 *
+	 */
+	~LocationIterator();
 
-		/**
-		 * @brief Shifts current pointer to it's right neighbour
-		 * 
-		 */
-		void right();	
+	/**
+	 * @brief Sets current to the next location in the sequential iteration of the map
+	 * 
+	 * Exceptions : out_of_range if there is no next location
+	 *
+	 */
+	void next();
 
-		/**
-		 * @brief Shifts current pointer to it's left neighbour
-		 * 
-		 */	
-		void left();
+	/**
+	 * @brief Sets current to the top left location of the map
+	 *
+	 */
+	void first();
 
-		/**
-		 * @brief Shifts current pointer to it's bottom neighbour
-		 * 
-		 */		
-		void down();
+	/**
+	 * @brief Returns true if sequential iteration is complete and false otherwise
+	 *
+	 * @return boolean
+	 */
+	bool isDone();
 
-		/**
-		 * @brief Shifts current pointer to it's top neighbour
-		 * 
-		 */		
-		void up();	
+	/**
+	 * @brief Returns the pointer to the current location
+	 *
+	 * @return Location*
+	 */
+	Location *getCurrent();
 
-		/**
-		 * @brief Returns true if sequential iteration is complete and false otherwise
-		 * 
-		 * @return boolean 
-		 */
-		bool isDone();	
-			
-		/**
-		 * @brief Returns the pointer to the current location
-		 * 
-		 * @return Location* 
-		 */
-		Location* getCurrent();
+protected:
+	Location *current;
+	bool hasNext();
+	bool movRight;
 };
 
 #endif
