0e75002cd33270460bcc16bd1747abb2528bd847
JulianPienaar <julian29pienaar@gmail.com>
2022-10-29 19:23:26 +0200

    Made test more extensive.

--- test/MapTest.cpp
+++ test/MapTest.cpp
@@ -1,12 +1,20 @@
 #include <limits.h>
+#include <iostream>
 #include <stdexcept>
 #include "../src/Map.h"
 #include "gtest/gtest.h"
 
 namespace {
 TEST(MapTest, Constructor){
-   /* Map* m=new Map();
-    delete m;*/
+    Map* m=new Map();
+    m->printMap();
+    Location* l=m->getTopLeft();
+    int i=0;
+    while(l->hasRight()){
+        std::cout<<i++;
+        l=l->getRight();
+    }
+    delete m;
 }
     
 }
