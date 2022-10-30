0131cf8089b7e64c5086c289083ce05bc86004c4
Jake Weatherhead <jakeweatherhead1@gmail.com>
2022-10-30 19:17:07 +0200

    Implemented basic class specification

--- src/LateOffensiveStrategy.h
+++ src/LateOffensiveStrategy.h
@@ -1,6 +1,16 @@
 #ifndef LO_STRATEGY
 #define LO_STRATEGY
 
+#include "Strategy.h"
 
+class Country;
+
+class LateOffensiveStrategy : public Strategy
+{
+public:
+    LateOffensiveStrategy();
+    ~LateOffensiveStrategy();
+    void takeTurn(Country* countryB);
+};
 
 #endif
