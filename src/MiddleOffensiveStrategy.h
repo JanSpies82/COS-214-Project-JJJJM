712c06fd95b616a8fe6e30e2bd809519af3f3b0f
Jake Weatherhead <jakeweatherhead1@gmail.com>
2022-10-30 19:20:23 +0200

    Implemented basic class specification

--- src/MiddleOffensiveStrategy.h
+++ src/MiddleOffensiveStrategy.h
@@ -1,6 +1,14 @@
 #ifndef MO_STRATEGY
 #define MO_STRATEGY
 
+#include "Strategy.h"
 
+class MiddleOffensiveStrategy : public Strategy
+{
+public:
+    MiddleOffensiveStrategy();
+    ~MiddleOffensiveStrategy();
+    void takeTurn(Country* countryB);
+};
 
 #endif
