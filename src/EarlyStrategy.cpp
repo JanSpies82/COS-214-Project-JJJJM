ba5098b6b24c5a51849a54f84860f16a74bc72d3
Jake Weatherhead <jakeweatherhead1@gmail.com>
2022-10-31 16:16:04 +0200

    Changed names of action functions

--- src/EarlyStrategy.cpp
+++ src/EarlyStrategy.cpp
@@ -1,5 +1,6 @@
 
 #include "EarlyStrategy.h"
+#include <iostream>
 
 EarlyStrategy::EarlyStrategy() 
 {
@@ -13,21 +14,28 @@ EarlyStrategy::~EarlyStrategy()
 
 void EarlyStrategy::takeTurn(double* strengthRatings, Country* countryA, Country* countryB) 
 {
+  double powerBalance = strengthRatings[0] - strengthRatings[1];
+  if (powerBalance < -0.5) 
+    negotiate(countryA, countryB);
+  if (powerBalance < 0.5 && powerBalance > -0.5) 
+    taxImports(countryA, countryB);
+  if (powerBalance > 0.5) 
+    banImports(countryA, countryB);
+}
 
+void EarlyStrategy::negotiate(Country* countryA, Country* countryB) 
+{
+  std::cout << "EarlyStrategy::negotiate selected" << std::endl;
 
 }
 
-void EarlyStrategy::implementDefense(double* strengthRatings, Country* countryA, Country* countryB) 
+void EarlyStrategy::taxImports(Country* countryA, Country* countryB) 
 {
+  std::cout << "EarlyStrategy::taxImports selected" << std::endl;
 
 }
 
-void EarlyStrategy::implementNeutral(double* strengthRatings, Country* countryA, Country* countryB) 
+void EarlyStrategy::banImports(Country* countryA, Country* countryB) 
 {
-
-}
-
-void EarlyStrategy::implementOffense(double* strengthRatings, Country* countryA, Country* countryB) 
-{
-
+  std::cout << "EarlyStrategy::banImports selected" << std::endl;
 }
