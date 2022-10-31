47aebce04101c4279bd47a109d84eadef248f043
Jake Weatherhead <jakeweatherhead1@gmail.com>
2022-10-31 17:03:17 +0200

    Implemented Action Functions

--- src/EarlyStrategy.cpp
+++ src/EarlyStrategy.cpp
@@ -1,5 +1,6 @@
 
 #include "EarlyStrategy.h"
+#include "Country.h"
 #include <iostream>
 
 EarlyStrategy::EarlyStrategy() 
@@ -9,7 +10,7 @@ EarlyStrategy::EarlyStrategy()
 
 EarlyStrategy::~EarlyStrategy() 
 {
-
+  
 }
 
 void EarlyStrategy::takeTurn(double* strengthRatings, Country* countryA, Country* countryB) 
@@ -26,16 +27,109 @@ void EarlyStrategy::takeTurn(double* strengthRatings, Country* countryA, Country
 void EarlyStrategy::negotiate(Country* countryA, Country* countryB) 
 {
   std::cout << "EarlyStrategy::negotiate selected" << std::endl;
+  double randomOutcome = (double)rand() / (double)RAND_MAX;
+  // CountryA chance of success : 0.3
+  // CountryB chance of success : 0.7
+  if (randomOutcome <= 0.3) // the negotiation has succeeded in easing tension
+  {
+    countryA->setPoliticalStability(countryA->getPoliticalStability() * 0.9);
+    countryA->setDomesticMorale(countryA->getDomesticMorale() * 0.9);
+    countryA->setBorderStrength(countryA->getBorderStrength() * 0.95);
+    countryA->setCapitalSafety(countryA->getCapitalSafety() * 0.95);
+    countryA->setWarSentiment(countryA->getWarSentiment() * 0.95);
+    countryA->setTradeRouteSafety(countryA->getTradeRouteSafety() * 0.95); 
 
+    countryB->setPoliticalStability(countryB->getPoliticalStability() * 0.9);
+    countryB->setDomesticMorale(countryB->getDomesticMorale() * 0.9);
+    countryB->setBorderStrength(countryB->getBorderStrength() * 0.9);
+    countryB->setCapitalSafety(countryB->getCapitalSafety() * 0.9);
+    countryB->setWarSentiment(countryB->getWarSentiment() * 0.9);
+    countryB->setTradeRouteSafety(countryB->getTradeRouteSafety() * 0.9);
+    return;
+  }
+  // CountryB feels antagonised by CountryA, reacts with aggression
+  countryA->setPoliticalStability(countryA->getPoliticalStability() * 0.9);
+  countryA->setDomesticMorale(countryA->getDomesticMorale() * 0.9);
+  countryA->setBorderStrength(countryA->getBorderStrength() * 0.95);
+  countryA->setCapitalSafety(countryA->getCapitalSafety() * 0.95);
+  countryA->setWarSentiment(countryA->getWarSentiment() * 0.95);
+  countryA->setTradeRouteSafety(countryA->getTradeRouteSafety() * 0.95); 
+
+  countryB->setPoliticalStability(countryB->getPoliticalStability() * 0.65);
+  countryB->setDomesticMorale(countryB->getDomesticMorale() * 0.65);
+  countryB->setBorderStrength(countryB->getBorderStrength() * 0.65);
+  countryB->setCapitalSafety(countryB->getCapitalSafety() * 0.65);
+  countryB->setWarSentiment(countryB->getWarSentiment() * 0.65);
+  countryB->setTradeRouteSafety(countryB->getTradeRouteSafety() * 0.65);
 }
 
 void EarlyStrategy::taxImports(Country* countryA, Country* countryB) 
 {
   std::cout << "EarlyStrategy::taxImports selected" << std::endl;
+  double randomOutcome = (double)rand() / (double)RAND_MAX;
+  // CountryA chance of success : 0.5
+  // CountryB chance of success : 0.5
+  if (randomOutcome <= 0.5) // CountryB learns its lesson, the measure was a success
+  {
+    countryA->setPoliticalStability(countryA->getPoliticalStability() * 0.9);
+    countryA->setDomesticMorale(countryA->getDomesticMorale() * 0.9);
+    countryA->setBorderStrength(countryA->getBorderStrength() * 0.95);
+    countryA->setCapitalSafety(countryA->getCapitalSafety() * 0.95);
+    countryA->setWarSentiment(countryA->getWarSentiment() * 0.95);
+    countryA->setTradeRouteSafety(countryA->getTradeRouteSafety() * 0.95); 
 
+    countryB->setPoliticalStability(countryB->getPoliticalStability() * 0.9);
+    countryB->setDomesticMorale(countryB->getDomesticMorale() * 0.9);
+    countryB->setBorderStrength(countryB->getBorderStrength() * 0.9);
+    countryB->setCapitalSafety(countryB->getCapitalSafety() * 0.9);
+    countryB->setWarSentiment(countryB->getWarSentiment() * 0.9);
+    countryB->setTradeRouteSafety(countryB->getTradeRouteSafety() * 0.9);
+    return;
+  }
+  // CountryB feels a line has been crossed, reacts with aggression
+  countryA->setPoliticalStability(countryA->getPoliticalStability() * 0.9);
+  countryA->setDomesticMorale(countryA->getDomesticMorale() * 0.9);
+  countryA->setBorderStrength(countryA->getBorderStrength() * 0.95);
+  countryA->setCapitalSafety(countryA->getCapitalSafety() * 0.95);
+  countryA->setWarSentiment(countryA->getWarSentiment() * 0.95);
+  countryA->setTradeRouteSafety(countryA->getTradeRouteSafety() * 0.95);
+
+  countryB->setPoliticalStability(countryB->getPoliticalStability() * 0.65);
+  countryB->setDomesticMorale(countryB->getDomesticMorale() * 0.65);
+  countryB->setBorderStrength(countryB->getBorderStrength() * 0.65);
+  countryB->setCapitalSafety(countryB->getCapitalSafety() * 0.65);
+  countryB->setWarSentiment(countryB->getWarSentiment() * 0.65);
+  countryB->setTradeRouteSafety(countryB->getTradeRouteSafety() * 0.65);
 }
 
 void EarlyStrategy::banImports(Country* countryA, Country* countryB) 
 {
   std::cout << "EarlyStrategy::banImports selected" << std::endl;
+  double randomOutcome = (double)rand() / (double)RAND_MAX;
+  // CountryA chance of success : 0.7
+  // CountryB chance of success : 0.3
+  if (randomOutcome <= 0.7) // CountryB learns its lesson, the measure was a success
+  {
+    countryA->setPoliticalStability(countryA->getPoliticalStability() * 0.9);
+    countryA->setDomesticMorale(countryA->getDomesticMorale() * 0.9);
+    countryA->setBorderStrength(countryA->getBorderStrength() * 0.95);
+    countryA->setCapitalSafety(countryA->getCapitalSafety() * 0.95);
+    countryA->setWarSentiment(countryA->getWarSentiment() * 0.95);
+    countryA->setTradeRouteSafety(countryA->getTradeRouteSafety() * 0.95); 
+
+    countryB->setPoliticalStability(countryB->getPoliticalStability() * 0.9);
+    countryB->setDomesticMorale(countryB->getDomesticMorale() * 0.9);
+    countryB->setBorderStrength(countryB->getBorderStrength() * 0.9);
+    countryB->setCapitalSafety(countryB->getCapitalSafety() * 0.9);
+    countryB->setWarSentiment(countryB->getWarSentiment() * 0.9);
+    countryB->setTradeRouteSafety(countryB->getTradeRouteSafety() * 0.9);
+    return;
+  }
+  // CountryB feels a line has been crossed, reacts with aggression
+  countryA->setPoliticalStability(countryA->getPoliticalStability() * 0.9);
+  countryA->setDomesticMorale(countryA->getDomesticMorale() * 0.9);
+  countryA->setBorderStrength(countryA->getBorderStrength() * 0.95);
+  countryA->setCapitalSafety(countryA->getCapitalSafety() * 0.95);
+  countryA->setWarSentiment(countryA->getWarSentiment() * 0.95);
+  countryA->setTradeRouteSafety(countryA->getTradeRouteSafety() * 0.95);
 }
