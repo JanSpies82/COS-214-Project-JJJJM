36e2afffd53cdb1afabe4f48c77623c626010595
DeadpanSplash82 <janco.spies@gmail.com>
2022-10-31 15:00:02 +0200

    Adapted test to be compatible with map implementation

--- test/SimulationStateTest.cpp
+++ test/SimulationStateTest.cpp
@@ -6,13 +6,39 @@
 #include "../src/MilitaryState.h"
 #include "gtest/gtest.h"
 
+
+// class IteratorTest : public testing::Test
+// {
+// public:
+//     virtual void SetUp() override
+//     {
+//         map = new Map();
+//         // country = new Country();
+//         // military = new Military();
+//         mapState = new MapState(map);
+//     }
+//     virtual void TearDown() override
+//     {
+//         delete map;
+//         // delete country;
+//         // delete military;
+//     }
+
+//     Map* map;
+//     Country* country;
+//     Military* military;
+//     MapState* mapState;
+//     CountryState* countryState;
+//     MilitaryState* militaryState;
+// };
+
 namespace
 {
-    class FakeMapState : public MapState
-    {
-    public:
-        FakeMapState() : MapState(NULL) {}
-    };
+    // class FakeMapState : public MapState
+    // {
+    // public:
+    //     FakeMapState() : MapState(NULL) {}
+    // };
     class FakeCountryState : public CountryState
     {
     public:
@@ -36,23 +62,29 @@ namespace
     TEST(SimulationStateTest, SetMapState)
     {
         SimulationState *simulationState = new SimulationState(NULL);
-        MapState *map1 = new FakeMapState();
+        Map* m1 = new Map();
+        MapState *map1 = new MapState(m1);
         simulationState->setMapState(map1);
         EXPECT_EQ(simulationState->getMapState(), map1);
-        MapState *map2 = new FakeMapState();
+        Map *m2 = new Map();
+        MapState *map2 = new MapState(m2);
         simulationState->setMapState(map2);
         EXPECT_EQ(simulationState->getMapState(), map2);
         delete simulationState;
+        delete m1;
+        delete m2;
     }
 
     TEST(SimulationStateTest, GetMapState)
     {
         SimulationState *simulationState = new SimulationState(NULL);
-        EXPECT_THROW(simulationState->getMapState(), out_of_range);
-        MapState *map1 = new FakeMapState();
+        EXPECT_THROW(simulationState->getMapState(), std::out_of_range);
+        Map* m1 = new Map();
+        MapState *map1 = new MapState(m1);
         simulationState->setMapState(map1);
         EXPECT_EQ(simulationState->getMapState(), map1);
         delete simulationState;
+        delete m1;
     }
 
     TEST(SimulationStateTest, AddCountryState)
@@ -73,16 +105,16 @@ namespace
     TEST(SimulationStateTest, GetCountryState)
     {
         SimulationState *simulationState = new SimulationState(NULL);
-        EXPECT_THROW(simulationState->getCountryState(0), out_of_range);
+        EXPECT_THROW(simulationState->getCountryState(0), std::out_of_range);
         CountryState *countryState1 = new FakeCountryState();
         simulationState->addCountryState(countryState1);
         EXPECT_EQ(simulationState->getCountryState(0), countryState1);
-        EXPECT_THROW(simulationState->getCountryState(1), out_of_range);
+        EXPECT_THROW(simulationState->getCountryState(1), std::out_of_range);
         CountryState *countryState2 = new FakeCountryState();
         simulationState->addCountryState(countryState2);
         EXPECT_EQ(simulationState->getCountryState(0), countryState1);
         EXPECT_EQ(simulationState->getCountryState(1), countryState2);
-        EXPECT_THROW(simulationState->getCountryState(2), out_of_range);
+        EXPECT_THROW(simulationState->getCountryState(2), std::out_of_range);
         delete simulationState;
     }
 
@@ -104,16 +136,16 @@ namespace
     TEST(SimulationStateTest, GetMilitaryState)
     {
         SimulationState *simulationState = new SimulationState(NULL);
-        EXPECT_THROW(simulationState->getMilitaryState(0), out_of_range);
+        EXPECT_THROW(simulationState->getMilitaryState(0), std::out_of_range);
         MilitaryState *militaryState1 = new FakeMilitaryState();
         simulationState->addMilitaryState(militaryState1);
         EXPECT_EQ(simulationState->getMilitaryState(0), militaryState1);
-        EXPECT_THROW(simulationState->getMilitaryState(1), out_of_range);
+        EXPECT_THROW(simulationState->getMilitaryState(1), std::out_of_range);
         MilitaryState *militaryState2 = new FakeMilitaryState();
         simulationState->addMilitaryState(militaryState2);
         EXPECT_EQ(simulationState->getMilitaryState(0), militaryState1);
         EXPECT_EQ(simulationState->getMilitaryState(1), militaryState2);
-        EXPECT_THROW(simulationState->getMilitaryState(2), out_of_range);
+        EXPECT_THROW(simulationState->getMilitaryState(2), std::out_of_range);
         delete simulationState;
     }
 }
