3773a4f5cb2247bbd00ecb9126f3a418cdadcfd2
Jake Weatherhead <jakeweatherhead1@gmail.com>
2022-10-31 16:31:56 +0200

    Implemented defensive middle strategy

--- src/MiddleStrategy.cpp
+++ src/MiddleStrategy.cpp
@@ -1,6 +1,8 @@
 #include "MiddleStrategy.h"
 #include "Country.h"
 
+#include <iostream>
+
 void MiddleStrategy::takeTurn(double* strengthRatings, Country* countryA, Country* countryB) 
 {
   double powerBalance = strengthRatings[0] - strengthRatings[1];
@@ -14,28 +16,52 @@ void MiddleStrategy::takeTurn(double* strengthRatings, Country* countryA, Countr
 
 void MiddleStrategy::defendBorders(Country* countryA, Country* countryB) 
 {
-  // countryB is unaffected by a negotitation with a weaker country
-
-  double responseCountryB = (double)rand() / (double)RAND_MAX;
-  if (responseCountryB >= 0.5) // countryB responds negatively
+  std::cout << "MiddleStrategy::defendBorders selected" << std::endl;
+  double randomOutcome = (double)rand() / (double)RAND_MAX;
+  // CountryA chance of success : 0.3
+  // CountryB chance of success : 0.7
+  if (randomOutcome <= 0.3) // CountryA successfully defended its borders
   {
     countryA->setPoliticalStability(countryA->getPoliticalStability() * 0.9);
     countryA->setDomesticMorale(countryA->getDomesticMorale() * 0.9);
     countryA->setBorderStrength(countryA->getBorderStrength() * 0.95);
     countryA->setCapitalSafety(countryA->getCapitalSafety() * 0.95);
     countryA->setWarSentiment(countryA->getWarSentiment() * 0.95);
-    countryA->setTradeRouteSafety(countryA->getTradeRouteSafety() * 0.95);
-    // no effect to countryA's selfReliance
+    countryA->setTradeRouteSafety(countryA->getTradeRouteSafety() * 0.95); 
+
+    countryB->setPoliticalStability(countryB->getPoliticalStability() * 0.65);
+    countryB->setDomesticMorale(countryB->getDomesticMorale() * 0.65);
+    countryB->setBorderStrength(countryB->getBorderStrength() * 0.65);
+    countryB->setCapitalSafety(countryB->getCapitalSafety() * 0.65);
+    countryB->setWarSentiment(countryB->getWarSentiment() * 0.65);
+    countryB->setTradeRouteSafety(countryB->getTradeRouteSafety() * 0.65);
+    return;
   }
-  // else no change to countryA's stats
+  // CountryB attacked CountryA's border/s and won
+  countryA->setPoliticalStability(countryA->getPoliticalStability() * 0.9);
+  countryA->setDomesticMorale(countryA->getDomesticMorale() * 0.9);
+  countryA->setBorderStrength(countryA->getBorderStrength() * 0.95);
+  countryA->setCapitalSafety(countryA->getCapitalSafety() * 0.95);
+  countryA->setWarSentiment(countryA->getWarSentiment() * 0.95);
+  countryA->setTradeRouteSafety(countryA->getTradeRouteSafety() * 0.95); 
+
+  countryB->setPoliticalStability(countryB->getPoliticalStability() * 0.65);
+  countryB->setDomesticMorale(countryB->getDomesticMorale() * 0.65);
+  countryB->setBorderStrength(countryB->getBorderStrength() * 0.65);
+  countryB->setCapitalSafety(countryB->getCapitalSafety() * 0.65);
+  countryB->setWarSentiment(countryB->getWarSentiment() * 0.65);
+  countryB->setTradeRouteSafety(countryB->getTradeRouteSafety() * 0.65);
 }
 
 void MiddleStrategy::airstrikeCapital(Country* countryA, Country* countryB) 
 {
+  std::cout << "MiddleStrategy::airstrikeCaptial selected" << std::endl;
 
 }
 
 void MiddleStrategy::launchInvasion(Country* countryA, Country* countryB) 
 {
+  std::cout << "MiddleStrategy::launchInvasion selected" << std::endl;
+
 
 }
