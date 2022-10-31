e5a65f4b8d0cac5f49b4704a53b870d46c85636d
Jake Weatherhead <jakeweatherhead1@gmail.com>
2022-10-31 18:28:54 +0200

    Commented out Strategy takeTurn()

--- src/Strategy.cpp
+++ src/Strategy.cpp
@@ -8,21 +8,21 @@ using namespace std;
 // 	throw "Not yet implemented";
 // }
 
-void Strategy::takeTurn(double* strengthRatings,Country* countryA, Country* countryB)
-{
-    //ifCountryA is weaker than CountryB
-    if(strengthRatings[0] < strengthRatings[1])
-    {
-        weakerMove(countryA,countryB);
-    }
-    //if CountryA is stronger than CountryB
-    else if(strengthRatings[0] > strengthRatings[1])
-    {
-        strongerMove(countryA,countryB);
-    }
-    //if CountryA is equal in strength to COuntryB
-    else{
-        equalStrength(countryA,countryB);
-    }
-}
+// void Strategy::takeTurn(double* strengthRatings,Country* countryA, Country* countryB)
+// {
+//     //ifCountryA is weaker than CountryB
+//     if(strengthRatings[0] < strengthRatings[1])
+//     {
+//         weakerMove(countryA,countryB);
+//     }
+//     //if CountryA is stronger than CountryB
+//     else if(strengthRatings[0] > strengthRatings[1])
+//     {
+//         strongerMove(countryA,countryB);
+//     }
+//     //if CountryA is equal in strength to COuntryB
+//     else{
+//         equalStrength(countryA,countryB);
+//     }
+// }
 
