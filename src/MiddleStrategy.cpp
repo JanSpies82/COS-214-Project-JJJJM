6469410cbf68e868c5a79156d0b475855288b1d5
Jake Weatherhead <jakeweatherhead1@gmail.com>
2022-10-31 16:36:04 +0200

    Implented remaining action functions

--- src/MiddleStrategy.cpp
+++ src/MiddleStrategy.cpp
@@ -56,12 +56,71 @@ void MiddleStrategy::defendBorders(Country* countryA, Country* countryB)
 void MiddleStrategy::airstrikeCapital(Country* countryA, Country* countryB) 
 {
   std::cout << "MiddleStrategy::airstrikeCaptial selected" << std::endl;
+  double randomOutcome = (double)rand() / (double)RAND_MAX;
+  // CountryA chance of success : 0.5
+  // CountryB chance of success : 0.5
+  if (randomOutcome <= 0.5) // CountryA successfully attacked CountryB's capital
+  {
+    countryA->setPoliticalStability(countryA->getPoliticalStability() * 0.9);
+    countryA->setDomesticMorale(countryA->getDomesticMorale() * 0.9);
+    countryA->setBorderStrength(countryA->getBorderStrength() * 0.95);
+    countryA->setCapitalSafety(countryA->getCapitalSafety() * 0.95);
+    countryA->setWarSentiment(countryA->getWarSentiment() * 0.95);
+    countryA->setTradeRouteSafety(countryA->getTradeRouteSafety() * 0.95); 
 
+    countryB->setPoliticalStability(countryB->getPoliticalStability() * 0.65);
+    countryB->setDomesticMorale(countryB->getDomesticMorale() * 0.65);
+    countryB->setBorderStrength(countryB->getBorderStrength() * 0.65);
+    countryB->setCapitalSafety(countryB->getCapitalSafety() * 0.65);
+    countryB->setWarSentiment(countryB->getWarSentiment() * 0.65);
+    countryB->setTradeRouteSafety(countryB->getTradeRouteSafety() * 0.65);
+    return;
+  }
+  // CountryB stopped the neutralised countryA's airforce
+  countryA->setPoliticalStability(countryA->getPoliticalStability() * 0.9);
+  countryA->setDomesticMorale(countryA->getDomesticMorale() * 0.9);
+  countryA->setBorderStrength(countryA->getBorderStrength() * 0.95);
+  countryA->setCapitalSafety(countryA->getCapitalSafety() * 0.95);
+  countryA->setWarSentiment(countryA->getWarSentiment() * 0.95);
+  countryA->setTradeRouteSafety(countryA->getTradeRouteSafety() * 0.95);
 }
 
 void MiddleStrategy::launchInvasion(Country* countryA, Country* countryB) 
 {
   std::cout << "MiddleStrategy::launchInvasion selected" << std::endl;
+  double randomOutcome = (double)rand() / (double)RAND_MAX;
+  // CountryA chance of success : 0.7
+  // CountryB chance of success : 0.3
+  if (randomOutcome <= 0.7) // CountryA successfully invaded CountryB
+  {
+    countryA->setPoliticalStability(countryA->getPoliticalStability() * 0.9);
+    countryA->setDomesticMorale(countryA->getDomesticMorale() * 0.9);
+    countryA->setBorderStrength(countryA->getBorderStrength() * 0.95);
+    countryA->setCapitalSafety(countryA->getCapitalSafety() * 0.95);
+    countryA->setWarSentiment(countryA->getWarSentiment() * 0.95);
+    countryA->setTradeRouteSafety(countryA->getTradeRouteSafety() * 0.95); 
 
+    countryB->setPoliticalStability(countryB->getPoliticalStability() * 0.65);
+    countryB->setDomesticMorale(countryB->getDomesticMorale() * 0.65);
+    countryB->setBorderStrength(countryB->getBorderStrength() * 0.65);
+    countryB->setCapitalSafety(countryB->getCapitalSafety() * 0.65);
+    countryB->setWarSentiment(countryB->getWarSentiment() * 0.65);
+    countryB->setTradeRouteSafety(countryB->getTradeRouteSafety() * 0.65);
+    return;
+  }
+  // CountryB successfully defended its territory
+  countryA->setPoliticalStability(countryA->getPoliticalStability() * 0.9);
+  countryA->setDomesticMorale(countryA->getDomesticMorale() * 0.9);
+  countryA->setBorderStrength(countryA->getBorderStrength() * 0.95);
+  countryA->setCapitalSafety(countryA->getCapitalSafety() * 0.95);
+  countryA->setWarSentiment(countryA->getWarSentiment() * 0.95);
+  countryA->setTradeRouteSafety(countryA->getTradeRouteSafety() * 0.95);
+
+  countryB->setPoliticalStability(countryB->getPoliticalStability() * 0.65);
+  countryB->setDomesticMorale(countryB->getDomesticMorale() * 0.65);
+  countryB->setBorderStrength(countryB->getBorderStrength() * 0.65);
+  countryB->setCapitalSafety(countryB->getCapitalSafety() * 0.65);
+  countryB->setWarSentiment(countryB->getWarSentiment() * 0.65);
+  countryB->setTradeRouteSafety(countryB->getTradeRouteSafety() * 0.65);
 
 }
