bb7c7f6535df872339b6fb8a626e875046f2b14d
Jake Weatherhead <jakeweatherhead1@gmail.com>
2022-11-01 21:46:25 +0200

    Updated MilitaryState.h

--- src/MilitaryState.h
+++ src/MilitaryState.h
@@ -4,6 +4,8 @@
 #define __MilitaryState_h__
 #include <vector>
 #include <exception>
+#include <stdexcept>
+#include <iostream>
 
 class Plane;
 class Ship;
@@ -13,70 +15,120 @@ class VehicleFactory;
 
 class MilitaryState
 {
+
 public:
+
   /**
-   * @brief construct a MilitaryState Object
-   **/
+   * @brief Default constructor
+   */
   MilitaryState();
 
+  /**
+   * @brief Default destructor
+   */
   ~MilitaryState();
 
   /**
-   * @brief getter for the numTroops attribute
-   * @return the number of troops of the military
-   */
+   * @brief setter for the ships in this military
+  */
+  void setShips(std::vector<Ship*>* _ships);
+
+  /**
+   * @brief setter for the planes in this military
+  */
+  void setPlanes(std::vector<Plane*>* _planes);
+
+  /**
+   * @brief setter for the tanks in this military
+  */
+  void setTanks(std::vector<Tank*>* _tanks);
+
+  /**
+   * @brief Setter for the battalions
+  */
+  void setBattalions(std::vector<Battalion*>* _battalions);
+
+  /**
+   * @brief setter for the VehicleFactories object
+  */
+  void setVehicleFactories(std::vector<VehicleFactory*>* _vehicleFactories);
+
+  /**
+   * @brief getter number of troops (integer)
+   * 
+   * @return number of troops in this military (integer)
+  */
   int getNumTroops();
 
   /**
-   * @brief setter for the numTroops attribute
-  */
-  void setNumTroops(int numTroops);
+   * @brief updates the number of troops in this military
+   */
+  void updateNumTroops(int _numTroops, bool isAddition);
 
   /**
-   * @brief getter for the numTanks attribute
-   * @return the number of tanks of the military
+   * @brief getter for the number of tanks in this military
+   * 
+   * @return the number of tanks in this military
   */
   int getNumTanks();
 
   /**
-   * @brief setter for the numTanks attribute
-   * @param numTanks the new number of tanks
-  */
-  void setNumTanks(int numTanks);
+   * @brief updates the number of tanks through addition or subtraction
+   * 
+   * @param _numTanks the number of tanks to add or subtract
+   * 
+   * @param isAddition true if the number of tanks is to be added, false if it is to be subtracted
+   */
+  void updateNumTanks(int _numTanks, bool isAddition);
 
   /**
-   * @brief getter for the number of planes of a country's military
-   * @return the number of planes of the military
-  */
-  int getNumPlanes();
-
-  /**
-   * @brief setter for the numPlanes attribute
-   * @param numPlanes the new number of planes
-  */
-  void setNumPlanes(int numPlanes);
-
-  /**
-   * @brief getter for the number of ships of a country's military
-   * @return the number of ships of the military
+   * @brief getter for the number of ships in this military
+   * 
+   * @return the number of ships in this military (integer)
   */
   int getNumShips();
 
   /**
-   * @brief setter for the numShips attribute
-   * @param numShips the new number of ships
+   * @brief updates the number of ships through addition or subtraction
+   * 
+   * @param _numShips the number of ships to add or subtract
+   * 
+   * @param isAddition true if the number of ships is to be added, false if it is to be subtracted
   */
-  void setNumShips(int numShips);
-  
-  Military* getState(); 
-  void setNumBattalions();
-  int getNumBatalions();		
+  void updateNumShips(int _numShips, bool isAddition);
+
+  /**
+   * @brief getter for the number of planes in this military
+   * 
+   * @return number of planes in this military (integer)
+  */
+  int getNumPlanes();
+
+  /**
+   * @brief Get the number of Vehicle Factories object
+  */
+  void updateNumPlanes(int _numPlanes, bool isAddition);
+
+  /**
+   * @brief Get the number of battalion objects in this military
+  */
+  int getNumBattalions();
+
+  /**
+   * @brief updates the number of battalions through addition or subtraction
+   * 
+   * @param _numBattalions the number of battalions to add or subtract
+   * 
+   * @param isAddition true if the number of battalions is to be added, false if it is to be subtracted
+   */
+  void updateNumBattalions(int _numBattalions, bool isAddition);
 
 private:
-	std::vector<Battalion*>* battalions;
-  std::vector<Ship*>* ships;
+  int numTroops;
   std::vector<Tank*>* tanks;
+  std::vector<Ship*>* ships;
   std::vector<Plane*>* planes;
+  std::vector<Battalion*>* battalions;
   std::vector<VehicleFactory*>* vehicleFactories;
 };
 
