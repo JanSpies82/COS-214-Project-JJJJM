08cced34f97ee5e8e8eb781d4ad90d2b45954cc0
DeadpanSplash82 <janco.spies@gmail.com>
2022-11-01 20:15:42 +0200

    Added functions for setting up country

--- src/SimulationManager.cpp
+++ src/SimulationManager.cpp
@@ -11,35 +11,35 @@
 
 using namespace std;
 
-string mapVal[27]={
-        "000000000000000099000066",
-        "000000000000000996666666",
-        "000000000000009996666666",
-        "000000000000009966660066",
-        "000000000000099966666666",
-        "000000000000099906666666",
-        "000000000000999906666666",
-        "000000000009999006666666",
-        "000000000009999906666666",
-        "000000030009999900666666",
-        "000000330009099990777666",
-        "000000330000709007777766",
-        "000033330000770007777777",
-        "000033033000777777777777",
-        "000300333077777777777777",
-        "000003330447777777777777",
-        "000000004447777777777777",
-        "000004444444777777777777",
-        "000000444444887777777777",
-        "000000044444888877777777",
-        "002220044444880888777777",
-        "002222224440088008877755",
-        "002222222000008800855555",
-        "022222220000800888050055",
-        "022222200000800080055005",
-        "002222000000008880005000",
-        "000000000000000800000550",
-    };
+string mapVal[27] = {
+    "000000000000000099000066",
+    "000000000000000996666666",
+    "000000000000009996666666",
+    "000000000000009966660066",
+    "000000000000099966666666",
+    "000000000000099906666666",
+    "000000000000999906666666",
+    "000000000009999006666666",
+    "000000000009999906666666",
+    "000000030009999900666666",
+    "000000330009099990777666",
+    "000000330000709007777766",
+    "000033330000770007777777",
+    "000033033000777777777777",
+    "000300333077777777777777",
+    "000003330447777777777777",
+    "000000004447777777777777",
+    "000004444444777777777777",
+    "000000444444887777777777",
+    "000000044444888877777777",
+    "002220044444880888777777",
+    "002222224440088008877755",
+    "002222222000008800855555",
+    "022222220000800888050055",
+    "022222200000800080055005",
+    "002222000000008880005000",
+    "000000000000000800000550",
+};
 
 SimulationManager::SimulationManager()
 {
@@ -59,7 +59,8 @@ SimulationManager::~SimulationManager()
     delete backup;
 }
 
-void SimulationManager::saveState(){
+void SimulationManager::saveState()
+{
     SimulationState *state = new SimulationState(this);
     state->setMapState(map->getState());
     state->addSuperpowerState(superpowers->at(0)->getState());
@@ -68,16 +69,16 @@ void SimulationManager::saveState(){
     backup->addMemento(new Memento(state));
 }
 
-void SimulationManager::setSuperpowers(){
+void SimulationManager::setSuperpowers()
+{
     superpowers->push_back(new Superpower("Allies"));
     superpowers->push_back(new Superpower("Axis Powers"));
 
-
-    Country* uk = new Country(/*"United Kingdom"*/);
+    Country *uk = new Country(/*"United Kingdom"*/);
     Country *france = new Country(/*"France"*/);
     Country *balkans = new Country(/*"Balkans"*/);
     Country *spainPortugal = new Country(/*"Spain/Portugal"*/);
-    Country *eastEurope = new Country(/*"Eastern Europe"*/);
+    Country *sovietUnion = new Country(/*"Soviet Union"*/);
     Country *scandanavia = new Country(/*"Scandanavia"*/);
 
     Country *germany = new Country(/*"Germany"*/);
@@ -89,20 +90,26 @@ void SimulationManager::setSuperpowers(){
     superpowers->at(0)->addCountry(france);
     superpowers->at(0)->addCountry(balkans);
     superpowers->at(0)->addCountry(spainPortugal);
-    superpowers->at(0)->addCountry(eastEurope);
+    superpowers->at(0)->addCountry(sovietUnion);
     superpowers->at(0)->addCountry(scandanavia);
 
     superpowers->at(1)->addCountry(germany);
     superpowers->at(1)->addCountry(italy);
 }
 
-void SimulationManager::setUpUK(Country *_uk){
-    vector<Location*> *ukLocations = new vector<Location*>();
+void SimulationManager::setUpUK(Country *_uk)
+{
+    vector<Location *> *ukLocations = new vector<Location *>();
     for (int i = 0; i < 27; i++)
         for (int j = 0; j < 25; j++)
-            if (mapVal[i][j] == '3'){
+            if (mapVal[i][j] == '3')
+            {
                 ukLocations->push_back(map->getLocation(j, i));
-                // map->getLocation(j, i)->setOwnedBy(_uk);
+                map->getLocation(j, i)->setOwnedBy(_uk);
+                if (map->getLocation(j, i)->getIsCapital())
+                {
+                    // _uk->setCapital(map->getLocation(j, i));
+                }
             }
     // _uk->setLocations(ukLocations);
 
