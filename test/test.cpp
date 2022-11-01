51b95b2aeec5041b49f5737a3f5399d4cd8dea19
DeadpanSplash82 <janco.spies@gmail.com>
2022-11-01 07:47:55 +0200

    Separated tests from main program

---
+++ test/test.cpp
@@ -0,0 +1,39 @@
+#include <gtest/gtest.h>
+
+#include "BackupTest.cpp"
+#include "BattalionTest.cpp"
+#include "BattalionObserverTest.cpp"
+#include "CountryTest.cpp"
+#include "CountryObserverTest.cpp"	
+#include "CountryMediatorTest.cpp"
+#include "CountryStateTest.cpp"
+#include "MementoTest.cpp"
+#include "MilitaryTest.cpp"
+#include "MilitaryStateTest.cpp"
+#include "PlaneFactoryTest.cpp"
+#include "PlaneTest.cpp"
+#include "ShipFactoryTest.cpp"
+#include "ShipTest.cpp"
+#include "SimulationManagerTest.cpp"
+#include "SimulationStateTest.cpp"
+#include "SuperpowerTest.cpp"
+#include "TankFactoryTest.cpp"
+#include "TankTest.cpp"
+#include "VehicleObserverTest.cpp"
+#include "RightNeighbourTest.cpp"
+#include "LeftNeighbourTest.cpp"
+#include "TopNeighbourTest.cpp"
+#include "BottomNeighbourTest.cpp"
+#include "TerritoryTest.cpp"
+#include "LocationIteratorTest.cpp"
+#include "MapTest.cpp"
+#include "MapStateTest.cpp"
+
+
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    //Terminal command for valgrind:
+    //valgrind --tool=memcheck --leak-check=yes --track-origins=yes --log-file=valg.txt ./test
+    return RUN_ALL_TESTS();
+}
