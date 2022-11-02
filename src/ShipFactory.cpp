0a73738ea709d04b387cc4dfaef772480a9006a5
Jake Weatherhead <jakeweatherhead1@gmail.com>
2022-11-02 10:44:38 +0200

    Commented out file

--- src/ShipFactory.cpp
+++ src/ShipFactory.cpp
@@ -1,22 +1,22 @@
-#include <exception>
-using namespace std;
-#include"Ship.h"
-#include "ShipFactory.h"
-#include "VehicleFactory.h"
+// #include <exception>
+// using namespace std;
+// #include"Ship.h"
+// #include "ShipFactory.h"
+// #include "VehicleFactory.h"
 
-		ShipFactory::ShipFactory(){
+// 		ShipFactory::ShipFactory(){
 
-		}
+// 		}
 
-		ShipFactory::~ShipFactory(){
+// 		ShipFactory::~ShipFactory(){
 
-		}
+// 		}
 
-		Vehicle* ShipFactory::manufactureVehicle(int damageBatallion,int damageToShip){
-			return new Ship(damageBatallion,damageToShip);
-		}
+// 		Vehicle* ShipFactory::manufactureVehicle(int damageBatallion,int damageToShip){
+// 			return new Ship(damageBatallion,damageToShip);
+// 		}
 
-		Vehicle* ShipFactory::manufactureVehicle(){
-			return new Ship();
-		}
+// 		Vehicle* ShipFactory::manufactureVehicle(){
+// 			return new Ship();
+// 		}
 
