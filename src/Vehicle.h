52325764a2f707d685dfdd2ffe8814bf253b0c82
Jake Weatherhead <jakeweatherhead1@gmail.com>
2022-11-02 14:52:19 +0200

    Fixed formatting

--- src/Vehicle.h
+++ src/Vehicle.h
@@ -1,26 +1,21 @@
-#include <exception>
-using namespace std;
-
 #ifndef __Vehicle_h__
 #define __Vehicle_h__
-#include"Country.h"//every vehicle is going to use attack to attack each Country
-	/**
-	 * @brief The Vehicle class is an abstract class of the different vehicle. 
-	 * derived Vehicles inherit from the Vehicle class
-	 **/
- class Vehicle
-{
-	
-	public:
-		/**
-		 * @brief constructs a Vehicle object
-		 **/ 
-		Vehicle();
-		/**
-		 * @brief destroys a Vehicle object
-		 **/
-		virtual ~Vehicle();
 
+#include "Country.h"
+#include <exception>
+
+class Vehicle
+{
+
+public:
+	/**
+	 * @brief constructs a Vehicle object
+	 **/
+	Vehicle();
+	/**
+	 * @brief destroys a Vehicle object
+	 **/
+	virtual ~Vehicle();
 };
 
 #endif
