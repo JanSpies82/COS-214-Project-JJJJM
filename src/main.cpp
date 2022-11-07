b51de8d58ad833b6887a578cf6e09793ea9608ee
JulianPienaar <104741835+JulianPienaar@users.noreply.github.com>
2022-11-07 10:26:31 +0200

    Removed author tag fixed doxygen

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
-/// @author Janco Spies
-
-//https://github.com/DeadpanSplash82/COS-214-Project-JJJJM
-
+/*! \mainpage GitHub Repository
+* <a href="https://github.com/DeadpanSplash82/COS-214-Project-JJJJM">Link to GitHub repository</a>
+*/
 #include "SimulationManager.h"
 #include <iostream>
 
